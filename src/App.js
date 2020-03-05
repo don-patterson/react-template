ef2ddf836722cd7881feb8430d2fdcf00e676cd3
Donald Patterson <donald.patterson@gmail.com>
2020-03-04 21:59:13 -0400

    forgot lint!

--- src/App.js
+++ src/App.js
@@ -4,19 +4,15 @@ import {withTheme} from "./theme";
 
 const useStyles = makeStyles({
   root: {
-    height: '100vh',
-    width: '100vw',
-    backgroundColor: 'orange',
+    height: "100vh",
+    width: "100vw",
+    backgroundColor: "orange",
   },
 });
 
 const App = () => {
   const classes = useStyles();
-  return (
-    <div className={classes.root}>
-      TEST 1 2
-    </div>
-  );
+  return <div className={classes.root}>TEST 1 2</div>;
 };
 
 export default withTheme(App);
